df8a482ac6788045ae5867e0af2a254b27c073cf
JoeCrow00 <joecrowton@hotmail.com>
2021-10-05 15:11:09 +0100

    added arrays and random generator

--- script.js
+++ script.js
@@ -1,2 +1,11 @@
 
+// Random generator
+function genRandom (num) {
+    return Math.floor(Math.random(num + 1));
+}
 
+// Arrays
+const mainProtein = ['Chicken breast', 'Ham', 'Tuna', 'Baked Beans', 'Egg', 'Smoked Salmon', 'Veggie Sausage'];
+const extraElement = ['mushrooms', 'avocado', 'egg', 'tomatoes', 'chedder cheese', 'spinach'];
+const carb = ['on toast', 'sandwich', 'rice bowl', 'baked potato', 'salad', 'pasta pot'];
+const extraFlavour = ['light mayo', 'HP sauce', 'hot sauce', 'greek yogurt', 'cream cheese', 'crumbled feta', 'parmesan'];
